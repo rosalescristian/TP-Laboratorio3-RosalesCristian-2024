4b03fae81380674f4b800f1ac93d39923b30c715
Cristian Rosales <rosalescristianm@gmail.com>
2024-07-07 20:32:07 -0300

    Added Templates in index.html \ template logic in scripts.js \ manteined styles in styles.css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,10 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     includeNavBar();
     includeHeader();
     includeFooter();
     loadContent();
 });
 
-// Genero la nav bar
 function generarNavBar() {
     const navBarHTML = `
         <nav>
@@ -19,23 +18,20 @@ function generarNavBar() {
     return navBarHTML;
 }
 
-// Genero el resto del contenido del Header en HTML
-function generarHTMLHeader(){
+function generarHTMLHeader() {
     const headerHTML = `
         <img class="logo" src="./images/logo.svg" alt="Logo Empresa" class="animado">
         <h1>Venta de Casas y Departamentos<br>Exclusivos de Lujo</h1>`;
     return headerHTML;
 }
 
-// Genero el contenido del Footer en HTML
-function generarHTMLFooter(){
+function generarHTMLFooter() {
     const footerHTML = `
         <p>Todos los derechos reservados 2024 &copy | By Cristian Rosales - UTN : Laboratorio III</p>`;
     return footerHTML;
 }
 
-// Inyecto la navbar en el header y el footer
-function includeNavBar(){
+function includeNavBar() {
     const header = document.getElementById('header');
     const footer = document.getElementById('footer');
     const navBar = generarNavBar();
@@ -44,87 +40,92 @@ function includeNavBar(){
     footer.innerHTML = navBar;
 }
 
-// Inyecto la navbar y el contenido del HTML en el header
 function includeHeader() {
     const header = document.getElementById('header');
     const headerContent = generarHTMLHeader();
     header.innerHTML += headerContent;
 }
 
-// Inyecto la navbar y el contenido del HTML en el footer
-function includeFooter(){
+function includeFooter() {
     const footer = document.getElementById('footer');
     const footerContent = generarHTMLFooter();
     footer.innerHTML += footerContent;
 }
 
-// Funciones para crear tarjetas y posts
-function createCard(imageSrc, imageAlt, title, description) {
-    return `
-        <article>
-            <img src="./images/${imageSrc}" alt="${imageAlt}">    
-            <h3>${title}</h3>
-            <p>${description}</p>
-        </article>
-    `;
+function updateCardElements(template, data) {
+    template.querySelector('img').src = `./images/${data.imageSrc}`;
+    template.querySelector('img').alt = data.imageAlt;
+    template.querySelector('h3').textContent = data.title;
+    template.querySelector('p').textContent = data.description;
 }
 
-function createPropertyCard(imageSrc, imageAlt, title, description, price, wc, parking, bedrooms) {
-    return `
-        <article>
-            <img src="./images/${imageSrc}" alt="${imageAlt}">
-            <h3>${title}</h3>
-            <p>${description}</p>
-            <p id="precio">${price}</p>
-            <div class="contenedor-iconos">
-                <span><img src="./images/icono_wc.svg" alt="Baños">${wc}</span>
-                <span><img src="./images/icono_estacionamiento.svg" alt="Estacionamiento">${parking}</span>
-                <span><img src="./images/icono_dormitorio.svg" alt="Dormitorios">${bedrooms}</span>
-            </div>
-            <button class="boton-grande-naranja">Ver Propiedad</button>
-        </article>
-    `;
+function createCard(templateId, data) {
+    const template = document.getElementById(templateId).content.cloneNode(true);
+    /* template.querySelector('img').src = `./images/${data.imageSrc}`;
+    template.querySelector('img').alt = data.imageAlt;
+    template.querySelector('h3').textContent = data.title;
+    template.querySelector('p').textContent = data.description; */
+    
+    updateCardElements(template, data);
+    
+    return template;
 }
 
-function createBlogPost(imageSrc, title, date, author, description) {
-    return `
-        <div class="container">
-            <div class="row my-5 border bg-light shadow">
-                <div class="col-md-6 bg-img" style="background-image: url(./images/${imageSrc})">
-                </div>
-                <div class="col-md-6 align-self-center p-4 ">
-                    <h3 class="text-dark">${title}</h3>
-                    <p class="lead">${date} por <span>${author}</span></p>
-                    <p>${description}</p>
-                    <a href="#" class="btn btn-outline-success btn-sm">Leer</a>
-                </div>
-            </div>
-        </div>
-    `;
+function createPropertyCard(templateId, data) {
+    const template = document.getElementById(templateId).content.cloneNode(true);
+    /* template.querySelector('img').src = `./images/${data.imageSrc}`;
+    template.querySelector('img').alt = data.imageAlt;
+    template.querySelector('h3').textContent = data.title;
+    template.querySelector('p').textContent = data.description; */
+
+    updateCardElements(template, data);
+    
+    template.querySelector('#precio').textContent = data.price;
+    const iconos = template.querySelector('.contenedor-iconos').children;
+    iconos[0].innerHTML += data.wc;
+    iconos[1].innerHTML += data.parking;
+    iconos[2].innerHTML += data.bedrooms;
+    return template;
 }
 
-// Función para cargar contenido
+function createBlogPost(templateId, data) {
+    const template = document.getElementById(templateId).content.cloneNode(true);
+    template.querySelector('.bg-img').style.backgroundImage = `url(./images/${data.imageSrc})`;
+    template.querySelector('h3').textContent = data.title;
+    template.querySelector('.lead').textContent = `${data.date} por ${data.author}`;
+    template.querySelectorAll('p')[1].textContent = data.description;
+    return template;
+}
+
 function loadContent() {
-    document.querySelector('.nosotros-tarjetas').innerHTML = `
-        ${createCard('icono1.svg', 'Icono Seguridad', 'Seguridad', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis a perspiciatis ducimus, cupiditate dicta distinctio pariatur ipsum dolores! Quia odio modi quis repellendus quo voluptatibus iure voluptas voluptates qui labore!')}
-        ${createCard('icono2.svg', 'Icono Precio', 'Mejor precio', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis a perspiciatis ducimus, cupiditate dicta distinctio pariatur ipsum dolores! Quia odio modi quis repellendus quo voluptatibus iure voluptas voluptates qui labore!')}
-        ${createCard('icono3.svg', 'Icono Tiempo', 'A tiempo', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis a perspiciatis ducimus, cupiditate dicta distinctio pariatur ipsum dolores! Quia odio modi quis repellendus quo voluptatibus iure voluptas voluptates qui labore!')}
-    `;
-
-    document.querySelector('.clasificados-tarjetas').innerHTML = `
-        ${createPropertyCard('anuncio1.jpg', 'Casa en el Lago', 'Casa de Lujo en el Lago', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ex minus? Tempora sit animi et maiores, cupiditate unde voluptas voluptates fuga nisi reiciendis nemo perferendis deserunt quidem suscipit asperiores voluptatibus!', 'U$S 350.000', 3, 3, 4)}
-        ${createPropertyCard('anuncio2.jpg', 'Casa moderna', 'Casa terminados de Lujo', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ex minus? Tempora sit animi et maiores, cupiditate unde voluptas voluptates fuga nisi reiciendis nemo perferendis deserunt quidem suscipit asperiores voluptatibus!','U$S 275.000', 3, 3, 4)}
-        ${createPropertyCard('anuncio3.jpg', 'Casa con Pileta', 'Casa Con Pileta', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ex minus? Tempora sit animi et maiores, cupiditate unde voluptas voluptates fuga nisi reiciendis nemo perferendis deserunt quidem suscipit asperiores voluptatibus!','U$S 250.000', 3, 3, 4)}
-    `;
-
+    
+    const nosotrosTarjetas = document.querySelector('.nosotros-tarjetas');
+    const clasificadosTarjetas = document.querySelector('.clasificados-tarjetas');
     const blogSection = document.querySelector('.blog');
-    blogSection.innerHTML = `
-        ${createBlogPost('blog1.jpg', 'Terraza en el techo de tu casa', '2024/05/01', 'Cristian', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..')}
-        ${createBlogPost('blog2.jpg', 'Guia para la decoracion de tu hogar', '2024/07/01', 'Cristian', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')}
-    `;
 
-    const comentariosSection = document.querySelector('.comentarios-tarjetas');
-    comentariosSection.innerHTML = `
-        <!-- Aquí puedes generar dinámicamente tus tarjetas de comentarios si lo deseas -->
-    `;
+    const templateIdNosotros = 'nosotros-template';
+    const templateIdClasificados = 'propiedad-template';
+    const templateIdBlog = 'blog-template';
+
+    const nosotrosData = [
+        { templateId: templateIdNosotros, imageSrc: 'icono1.svg', imageAlt: 'Icono Seguridad', title: 'Seguridad', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis a perspiciatis ducimus, cupiditate dicta distinctio pariatur ipsum dolores! Quia odio modi quis repellendus quo voluptatibus iure voluptas voluptates qui labore!' },
+        { templateId: templateIdNosotros, imageSrc: 'icono2.svg', imageAlt: 'Icono Precio', title: 'Mejor precio', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis a perspiciatis ducimus, cupiditate dicta distinctio pariatur ipsum dolores! Quia odio modi quis repellendus quo voluptatibus iure voluptas voluptates qui labore!' },
+        { templateId: templateIdNosotros, imageSrc: 'icono3.svg', imageAlt: 'Icono Tiempo', title: 'A tiempo', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis a perspiciatis ducimus, cupiditate dicta distinctio pariatur ipsum dolores! Quia odio modi quis repellendus quo voluptatibus iure voluptas voluptates qui labore!' }
+    ];
+
+    const clasificadosData = [
+        { templateId: templateIdClasificados, imageSrc: 'anuncio1.jpg', imageAlt: 'Casa en el Lago', title: 'Casa de Lujo en el Lago', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ex minus? Tempora sit animi et maiores, cupiditate unde voluptas voluptates fuga nisi reiciendis nemo perferendis deserunt quidem suscipit asperiores voluptatibus!', price: 'U$S 350.000', wc: 3, parking: 3, bedrooms: 4 },
+        { templateId: templateIdClasificados, imageSrc: 'anuncio2.jpg', imageAlt: 'Casa moderna', title: 'Casa terminados de Lujo', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ex minus? Tempora sit animi et maiores, cupiditate unde voluptas voluptates fuga nisi reiciendis nemo perferendis deserunt quidem suscipit asperiores voluptatibus!', price: 'U$S 275.000', wc: 3, parking: 3, bedrooms: 4 },
+        { templateId: templateIdClasificados, imageSrc: 'anuncio3.jpg', imageAlt: 'Casa con Pileta', title: 'Casa Con Pileta', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ex minus? Tempora sit animi et maiores, cupiditate unde voluptas voluptates fuga nisi reiciendis nemo perferendis deserunt quidem suscipit asperiores voluptatibus!', price: 'U$S 250.000', wc: 3, parking: 3, bedrooms: 4 }
+    ];
+
+    const blogData = [
+        { templateId: templateIdBlog, imageSrc: 'blog1.jpg', title: 'Terraza en el techo de tu casa', date: '2024/05/01', author: 'Cristian', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..' },
+        { templateId: templateIdBlog, imageSrc: 'blog2.jpg', title: 'Guia para la decoracion de tu hogar', date: '2024/07/01', author: 'Cristian', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }
+    ];
+
+    nosotrosData.forEach(item => nosotrosTarjetas.appendChild(createCard(item.templateId, item)));
+    clasificadosData.forEach(item => clasificadosTarjetas.appendChild(createPropertyCard(item.templateId, item)));
+    blogData.forEach(item => blogSection.appendChild(createBlogPost(item.templateId, item)));
+
 }
